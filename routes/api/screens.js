0a638933369deb84fc8d84a855db5b1e3214a2c2
gauravano <sachdeva.gaurav1997@gmail.com>
2018-10-15 00:07:30 +0530

    api for reserving ticket done

--- routes/api/screens.js
+++ routes/api/screens.js
@@ -64,71 +64,80 @@ route.post('/:screen_name/reserve', (req, res) => {
   var movie_name = req.params.screen_name;
   var movie_id;
 
-  // Movie.find({
-  //     where: {
-  //         name: movie_name
-  //     }
-  // }).then((movie) => {
-  //     if(movie){
-  //          movie_id  = movie.id;
-  //     }else{
-  //         return res.status(500).send({
-  //           message: "No movie with such name exist!"
-  //     });
-  // }}).catch((err) => {
-  //     return res.status(500).send({
-  //         message: "Could not fetch listings. Sorry :( "
-  //     });
-  // });
+  Movie.find({
+      where: {
+          name: movie_name
+      }
+  }).then((movie) => {
+      if(movie){
+           movie_id  = movie.id;
+           console.log(movie_id);
 
-  var seatA_res = req.body.seats.A;
-  var seatB_res = req.body.seats.B;
-  var seatD_res = req.body.seats.D;
+           var seatA_res = req.body.seats.A;
+           var seatB_res = req.body.seats.B;
+           var seatD_res = req.body.seats.D;
 
-  console.log(seatA_res," ",seatB_res," ",seatD_res);
+           console.log(seatA_res," ", seatB_res," ",seatD_res);
 
-  for(var i=0; i<3;i++){
-    if(i ==0 && seatA_res != "undefined"){
-      reserve_seat(movie_id, "A",seatA_res);
-    }else if(i == 1 && seatB_res != "undefined"){
-      reserve_seat(movie_id, "B",seatB_res);
-    }else if(i == 2 && seatD_res != "undefined"){
-      reserve_seat(movie_id, "D",seatD_res);
-    }else{
-      return res.status(500).send({
-          message: "Invalid request!"
+          var row_name;
+          var seats;
+
+           for(var i=0; i<3;i++){
+             if(i === 0 && Array.isArray(seatA_res)){
+               console.log("AAA");
+               row_name = "A";
+               seats = req.body.seats.A;
+               console.log("seats",seats);
+             }else if(i === 1 && Array.isArray(seatB_res)){
+               console.log("BBBBB");
+               row_name = "B";
+               seats = req.body.seats.B;
+             }else if(i === 2 && Array.isArray(seatD_res)){
+               console.log("DDDD");
+               row_name = "D";
+               seats = req.body.seats.D;
+             }else{
+               continue;
+             }
+
+             for(var seat of seats){
+               Seat.find({
+                   where: {
+                       movie_id: movie_id,
+                       row_name: row_name,
+                       seat_num: seat
+                   }
+               }).then((seat_status) => {
+                   if(seat_status){
+                      seat_status.updateAttributes({
+                        reserved: true
+                      })
+              }else{
+                       return res.status(500).send({
+                         message: "Seat not available!"
+                   });
+               }}).catch((err) => {
+                   return res.status(500).send({
+                       message: "There is error while booking tickets :( "
+                   });
+               });
+             }
+           }
+
+                 res.status(200).send({
+                   message: "Success :)"
+                 });
+      }else{
+          return res.status(500).send({
+            message: "No movie with such name exist!"
       });
-    }
-
-  }
+  }}).catch((err) => {
+      return res.status(500).send({
+          message: "Could not fetch listings. Sorry :( "
+      });
+  });
 
 });
-
-function reserve_seat(movie_id, row_name, seats){
-  console.log(movie_id, row_name, seats);
-  for(var seat of seats){
-    Seat.find({
-        where: {
-            movie_id: movie_id,
-            row_name: row_name,
-            seat_num: seat
-        }
-    }).then((seat_status) => {
-        if(seat_status){
-
-        }else{
-            return res.status(500).send({
-              message: "No movie with such name exist!"
-        });
-    }}).catch((err) => {
-        return res.status(500).send({
-            message: "Could not fetch listings. Sorry :( "
-        });
-    });
-  }
-}
-
-
 
 route.get('/filter', (req, res) => {
 
